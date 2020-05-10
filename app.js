721680d058f4792f2fe3018bfcbd29deeb51b4da
larry <dh821020@gmail.com>
2020-05-10 23:03:26 +0900

    EJS layout

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let item_list =  ["Buy Food","Cook Food","Eat Food"] ;
-
+let workItems =[]
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}))
@@ -22,14 +22,29 @@ app.get("/",function(req,res){
     };
     let day = today.toLocaleDateString("en-CA",options);
     
-    res.render("list",{kindOfDay:day,newListItem:item_list})
+    res.render("list",{listTitle:day,newListItem:item_list})
 });
 
 app.post("/",function(req,res){
+
     let item = req.body.newItem;
-    item_list.push(item);
-    res.redirect("/");
+    if(req.body.list == "Work List"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        item_list.push(item);
+        res.redirect("/");
+    }
 })
+
+app.get("/work",function(req,res){
+    res.render("list",{listTitle:"Work List",newListItem:workItems})
+})
+
+app.get("/about",function(req,res){
+    res.render('about')
+})
+
 
 app.listen(3000,function(){
     console.log("Server started on port 3000")
